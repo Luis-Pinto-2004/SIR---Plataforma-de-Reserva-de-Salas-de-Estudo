4c5d39709fac5f4dbdc9e598603e4712768ec96a
LilChyna <luis.pinto@ipvc.pt>
2026-01-13 12:10:37 +0000

    Fix route auth.js

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 
-const User = require('../models/User');
+const { User } = require('../models/User');
+
 const { signJwt, verifyJwt } = require('../utils/jwt');
 const { env } = require('../config/env');
 
